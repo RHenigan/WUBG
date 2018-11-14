f8f077fc21aaf61e5accb1e9b7a58b9fa72ecf96
RHeniz <heniganr1@gmail.com>
2018-11-13 22:27:14 -0500

    Update timing intervals

--- src/main.js
+++ src/main.js
@@ -5,8 +5,10 @@ const BrowserWindow = electron.BrowserWindow
 
 let mainWindow
 
-function startTimer(duration) {
-    var timer = duration, minutes, seconds;
+let TWO_HOURS = 60 * 120;
+
+function startTimer() {
+    var timer = TWO_HOURS, minutes, seconds;
     setInterval(function () {
         minutes = parseInt(timer / 60, 10);
         seconds = parseInt(timer % 60, 10);
@@ -14,11 +16,31 @@ function startTimer(duration) {
         minutes = minutes <10 ? "0" + minutes : minutes;
         seconds = seconds < 10 ? "0" + seconds : seconds;
 
-        console.log(minutes + ":" + seconds);
-
-        if (--timer < 0) {
-            timer = duration;
+        if(seconds == 0) {
+            console.log(minutes);
         }
+        if(minutes == 180) {
+            console.log("prepare for landing");
+        } else if(minutes == 150 && seconds == 0) {
+            console.log("Strom shrinking");
+        } else if(minutes == 120 && seconds == 0) {
+            console.log("Storm Shrunk");
+        } else if(minutes == 100 && seconds == 0) {
+            console.log("Storm shrinking");
+        } else if(minutes == 80 && seconds == 0) {
+            console.log("Strom shrunk");
+        } else if(minutes == 65 && seconds == 0) {
+            console.log("Storm shrinking");
+        } else if(minutes == 50 && seconds == 0) {
+            console.log("Storm shrunk");
+        } else if(minutes == 40 && seconds == 0) {
+            console.log("Strom shrinking");
+        } else if(minutes == 30 && seconds == 0) {
+            console.log("Storm shrunk");
+        }
+
+        --timer
+
     }, 1000);
 }
 
@@ -28,6 +50,5 @@ app.on('ready', _ => {
 		height: 500
 	})
 
-    var OneHundredEightyMinutes = 60 * 180;
-    startTimer(OneHundredEightyMinutes);
+    startTimer();
 })
