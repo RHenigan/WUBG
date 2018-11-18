cb79d2beeb256e59a8d3b18af209351eb24d993e
RHeniz <heniganr1@gmail.com>
2018-11-17 21:35:37 -0500

    Add secondary display check

--- main.js
+++ main.js
@@ -7,13 +7,24 @@ let mainWindow
 
 
 app.on('ready', _ => {
+    var electronScreen = electron.screen;
+    var displays = electronScreen.getAllDisplays();
+    var externalDisplay = null;
+    
+    for (i in displays) {
+        if (displays[i].bounds.x != 0 || displays[i].bounds.y != 0) {
+            externalDisplay = displays[i];
+            break;
+        }
+    }
+
+    if(externalDisplay) {
+        player_window = new BrowserWindow({})
+        player_window.setFullScreen(true);
+    }
+
 	dm_windows = new BrowserWindow({
 		width: 500,
 		height: 500
-    })
-    
-    player_window = new BrowserWindow({
-        width:500,
-        height:500
     })
 })
