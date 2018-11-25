958cf474b9e4d67a5fc30813049c9f29a3817ebe
Brian Anstett <baanstet@oakland.edu>
2018-11-24 23:39:29 -0500

    Something like this?
    @RHeniz

--- game_modules/game.js
+++ game_modules/game.js
@@ -1,18 +1,33 @@
-module.exports = function(){
-  // Game functions in here
+// module.exports = function(){
+//   // Game functions in here
 
-    gameTimer:function() {
-        var timer = 60 * 120, minutes, seconds;
-        setInterval(function () {
-            minutes = parseInt(timer / 60, 10);
-            seconds = parseInt(timer % 60, 10);
+//     gameTimer:function() {
+//         var timer = 60 * 120, minutes, seconds;
+//         setInterval(function () {
+//             minutes = parseInt(timer / 60, 10);
+//             seconds = parseInt(timer % 60, 10);
 
-            minutes = minutes < 10 ? "0" + minutes : minutes;
-            seconds = seconds < 10 ? "0" + seconds : seconds;
+//             minutes = minutes < 10 ? "0" + minutes : minutes;
+//             seconds = seconds < 10 ? "0" + seconds : seconds;
 
-            console.log(minutes + ":" + seconds);
+//             console.log(minutes + ":" + seconds);
             
-            --timer;
-        }, 1000);
-    }
+//             --timer;
+//         }, 1000);
+//     }
+// }
+
+module.exports.gameTimer = function(){
+    var timer = 60 * 120, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        console.log(minutes + ":" + seconds);
+
+        --timer;
+    }, 1000);
 }
