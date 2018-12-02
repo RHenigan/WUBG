e116ff0c1c266b1317876f484d62f832cb942aed
Brian Anstett <baanstet@oakland.edu>
2018-12-02 18:25:36 -0500

    few css tweaks

--- main.js
+++ main.js
@@ -1,19 +1,51 @@
-const electron = require('electron')
+const { app, BrowserWindow, dialog } = require('electron');
 const game = require("./game_modules/game");
-const app = electron.app
-const BrowserWindow = electron.BrowserWindow
+// const app = electron.app
+// const BrowserWindow = electron.BrowserWindow
 
-let mainWindow
+let dm_windows;
+let player_window;
+let exit_prompt = false;
 
 
 app.on('ready', _ => {
-	dm_windows = new BrowserWindow({
-		width: 500,
-		height: 500
+    dm_windows = new BrowserWindow({
+        width: 1300,
+        minWidth: 1300,
+        height: 1000
+
     })
-    
+
+    dm_windows.on('close', exit);
+
+    dm_windows.loadURL(`file://${__dirname}/assets/html/dm.html`);
+
     player_window = new BrowserWindow({
-        width:500,
-        height:500
+        width: 500,
+        height: 500
     })
+
+    player_window.on('close', exit)
 })
+
+
+// Support Functions
+function exit(event) {
+    if (!exit_prompt) {
+        event.preventDefault();
+        const options = {
+            type: 'info',
+            title: 'Exit WUBG?',
+            message: 'Are you sure you want to quit WUBG?',
+            buttons: ['Yes', 'No']
+        }
+        dialog.showMessageBox(options, index => {
+            if (index === 0) {
+                exit_prompt = true;
+                dm_windows = null;
+                player_window = null;
+                app.quit();
+            }
+        })
+    }
+}
